87fcac6cdeefc7be29e48f65f639400e3a45c7e5
Jared Hamby <irjamby@gmail.com>
2013-07-03 17:17:02 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playnvote::Application.config.secret_token = '04deb1c2cb1dcf21d2ac9468a6da3c3b423b0145511376002b6ee4664ce8dbdae7e4e0dc1b7ae2a1f14605e7e5ab77a3224bbce3e6f2b39cd3de2e74a3777ce9'
