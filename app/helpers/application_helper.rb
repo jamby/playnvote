16ffc394c5cf456672f956fdd6f91e72f97ab164
Jared Hamby <irjamby@gmail.com>
2013-07-05 16:26:43 -0400

    Added header bar with the active link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_header_link(*args)
+    # Must be passed in like [controller, action] for this logic to work
+    link_class = ''
+    args.each do |arg|
+      controller = arg[0]
+      action = arg[1]
+      link_class = 'active' if params[:action] == action and params[:controller] == controller
+    end
+    link_class
+  end
 end
