6c584eb8049509bcaa97c47a1cf1d23ab0167e7d
Jared Hamby <irjamby@gmail.com>
2013-07-12 18:05:16 -0400

    Added jQuery/JSON for voting and non-user interactions.

--- app/assets/javascripts/related_games.js
+++ app/assets/javascripts/related_games.js
@@ -2,17 +2,39 @@ $(document).ready( function() {
 	
 	$(".upvote").click(function(event) {
 		user_id = $(this).attr('user_id');
+		if (user_id == "") {
+			return;
+		}
 		game1_id = $(this).attr('game1_id');
 		game2_id = $(this).attr('game2_id');
-
-		$.post('/related_games/upvote', {user_id: user_id, game1_id: game1_id, game2_id: game2_id});
+		_this = $(this);
+		
+		$.post('/related_games/upvote', {user_id: user_id, game1_id: game1_id, game2_id: game2_id},
+			function(data) {
+				// update number
+				_this.siblings('.voting-info-numbers').html("<span class=\"upvote-number\">Upvotes: " + data.upvotes + " </span><span class=\"downvote-number\">Downvotes: " + data.downvotes + "</span>");
+			});
+		
+		$(this).siblings('.icon-arrow-down').removeClass('color-red');
+		$(this).toggleClass('color-green');
 	});
 	
 	$(".downvote").click(function(event) {
 		user_id = $(this).attr('user_id');
+		if (user_id == "") {
+			return;
+		}
 		game1_id = $(this).attr('game1_id');
 		game2_id = $(this).attr('game2_id');
-
-		$.post('/related_games/downvote', {user_id: user_id, game1_id: game1_id, game2_id: game2_id});
+		_this = $(this);
+		
+		$.post('/related_games/downvote', {user_id: user_id, game1_id: game1_id, game2_id: game2_id},
+			function(data) {
+				// update number
+				_this.siblings('.voting-info-numbers').html("<span class=\"upvote-number\">Upvotes: " + data.upvotes + " </span><span class=\"downvote-number\">Downvotes: " + data.downvotes + "</span>");
+			});
+		
+		$(this).siblings('.icon-arrow-up').removeClass('color-green');
+		$(this).toggleClass('color-red');
 	});
 })
--- app/controllers/related_games_controller.rb
+++ app/controllers/related_games_controller.rb
@@ -1,7 +1,7 @@
 class RelatedGamesController < ApplicationController
   respond_to :json
 
-  def upvote# in link... user: current_user, game1: game, game2: game compared
+  def upvote
     @user = User.find(params[:user_id])
     @game1 = Game.find(params[:game1_id])
     @game2 = Game.find(params[:game2_id])
@@ -13,9 +13,8 @@ class RelatedGamesController < ApplicationController
     else
       @user.up_votes @relatedGame
     end
-    
     respond_to do |format|
-      format.js
+      format.json { render json: { upvotes: @relatedGame.upvotes.size, downvotes: @relatedGame.downvotes.size }.to_json }
     end
   end
   
@@ -33,7 +32,7 @@ class RelatedGamesController < ApplicationController
     end
     
     respond_to do |format|
-      format.js
+      format.json { render json: { upvotes: @relatedGame.upvotes.size, downvotes: @relatedGame.downvotes.size }.to_json }
     end
   end
 
