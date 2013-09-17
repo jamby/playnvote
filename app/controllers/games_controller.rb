a8ee2139b4b1e92f2f0a3220b3b3b95c9e5e322b
Jared Hamby <irjamby@gmail.com>
2013-09-17 08:54:40 -0400

    Added ability to create related_games when one doesn't exist, by searching.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -23,18 +23,16 @@ class GamesController < ApplicationController
   end
   
   def search_related
-    puts "*" * 100
     @current_game = Game.find(params[:current_game])
-    puts @current_game.title
     @search_game = Game.find(params[:search_game_id])
-    puts @search_game.title
     
     @find_related = RelatedGame.where(game1_id: [@current_game.id, @search_game.id], game2_id: [@current_game.id, @search_game.id])
-    redirect_to related_game_path(@find_related.first.id)
-    if @find_related.nil?
-      puts "Doesn't exist"
-      # @new_related = RelatedGame.create(game1_id: @current_game.id, game2_id: @search.id)
-      # puts @new_related
+
+    if !@find_related.empty?
+      redirect_to related_game_path(@find_related.first.id)
+    else
+      @new_related = RelatedGame.create(game1_id: @current_game.id, game2_id: @search_game.id)
+      redirect_to related_game_path(@new_related.id)      
     end
   end
 end
