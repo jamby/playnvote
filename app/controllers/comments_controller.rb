5cea39c5976790afdaaedda33aeffcbcf8b7e892
Jared Hamby <irjamby@gmail.com>
2013-09-18 14:21:21 -0400

    Added deletion of root comments, unsure about children.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @relatedGame = RelatedGame.find(params[:related_game_id])
+    @all_comments = @relatedGame.comment_threads
     @comment = Comment.build_from(@relatedGame, current_user.id, params[:comment][:body])
     if @comment.save
       respond_to do |format|
@@ -9,6 +10,16 @@ class CommentsController < ApplicationController
     else
       flash.now[:error] = "Comment was not submitted."
       redirect_to root_path
+    end
+  end
+  
+  def destroy
+    @relatedGame = RelatedGame.find(params[:related_game_id])
+    @all_comments = @relatedGame.comment_threads
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      format.js
     end
   end
 end
