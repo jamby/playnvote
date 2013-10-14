5ae483a08b6e45144797fe9d41430b19e3ec1126
Jared Hamby <irjamby@gmail.com>
2013-10-14 13:43:37 -0400

    Added more to user's profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @comments = Comment.where(user_id: @user.id)
+    @votes = 0
+    Comment.where(user_id: @user.id).each{ |c| @votes += (c.upvotes.size - c.downvotes.size) }
+    @votes -= Comment.where(user_id: @user.id).size
   end
 end
