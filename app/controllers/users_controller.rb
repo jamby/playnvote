07182c5baea10ab395cc9ac4cbb9e8f56f0e49f8
Jared Hamby <irjamby@gmail.com>
2013-10-15 13:27:42 -0400

    Changed Karma to Points.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @comments = Comment.where(user_id: @user.id)
-    @karma = 0
-    @comments.each{ |c| @karma += (c.upvotes.size - c.downvotes.size) }
+    @points = 0
+    @comments.each{ |c| @points += (c.upvotes.size - c.downvotes.size) }
     @num_votes = @comments.size
-    @karma -= @num_votes
+    @points -= @num_votes
   end
 end
