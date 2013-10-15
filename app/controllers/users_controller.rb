4ac11bd23f47d463af9e8a7edf4d2297bf8f2aee
Jared Hamby <irjamby@gmail.com>
2013-10-15 09:06:31 -0400

    Added Total Comments and changed Votes to Karma (need different word).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @comments = Comment.where(user_id: @user.id)
-    @votes = 0
-    Comment.where(user_id: @user.id).each{ |c| @votes += (c.upvotes.size - c.downvotes.size) }
-    @votes -= Comment.where(user_id: @user.id).size
+    @karma = 0
+    @comments.each{ |c| @karma += (c.upvotes.size - c.downvotes.size) }
+    @num_votes = @comments.size
+    @karma -= @num_votes
   end
 end
